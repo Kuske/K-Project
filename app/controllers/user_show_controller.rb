c26b9226f3b1a2394944e80726ddb9b674e60b21
keidaroo <you@example.com>
2018-01-22 01:48:21 +0900

    add user_show controller

---
+++ app/controllers/user_show_controller.rb
@@ -0,0 +1,2 @@
+class UserShowController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user_show/admin1'
+  get 'user_show/admin2'
+  root'application#justsayinghello'
 end
