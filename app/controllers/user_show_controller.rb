083bd15320d15a9f66b1db51bd01368ec63ccfc8
keidaroo <keiforprogramming@gmail.com>
2018-01-28 15:58:53 +0900

    add comments

--- app/controllers/user_show_controller.rb
+++ app/controllers/user_show_controller.rb
@@ -1,6 +1,8 @@
 class UserShowController < ApplicationController
   def show
       @user=User.find_by(userid:params[:userid])
+      #useridがパラメーターの時のUser（構造体みたいなもの）をuserに代入
       render "user_show/user_show"
+      #viewsのuser_show.html.erbに移動
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get'/show/:userid'=>'user_show#show'
+  #アドレスがshow/numだったときにuseridにnumが代入され、user_show_controllerのshow関数に移動される
   root'application#justsayinghello'
+  #もしなにもなかったらapplication controllerのjustsaying hello関数に移動
 end
