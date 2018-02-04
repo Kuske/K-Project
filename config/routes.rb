f31576d46a4523f8e0db63c8972ba7cd896a5dcb
keidaroo <you@example.com>
2018-02-05 00:00:37 +0900

    add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   post 'user_show/update'=>'user_show#update'
   get'/show/:userid'=>'user_show#show'
   get'/signup'=>'user_show#signup'
-  root 'application#justsayinghello'
+  get '/home'=>'home#initial'
+  root 'home#initial'
 end
