2e4acf1ba0088f41cbadff4fd7579b91bb35f7eb
keidaroo <keiforprogramming@gmail.com>
2018-03-16 22:01:33 +0900

    with Kuske

--- db/migrate/20180222133819_create_groups.rb
+++ db/migrate/20180222133819_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :groups do |t|
       t.string :group_id
-      t.int[] :group_member
+      # t.int[] :group_member
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204180802) do
+ActiveRecord::Schema.define(version: 20180222133819) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer "userid"
