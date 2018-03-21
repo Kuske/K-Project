ba10b83c0733f66c9a2ed6395d9b6e4895960715
keidaroo <keiforprogramming@gmail.com>
2018-03-22 02:23:05 +0900

    changed login

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_222_133_819) do
-  create_table 'groups', force: :cascade do |t|
-    t.string 'group_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema.define(version: 20180222150009) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.integer 'userid'
-    t.string 'username'
-    t.text 'comment'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'password_digest'
+  create_table "messages", force: :cascade do |t|
+    t.integer "message_id"
+    t.text "message_text"
+    t.integer "message_user"
+    t.string "message_type"
+    t.string "message_group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "userid"
+    t.string "username"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+  end
+
 end
